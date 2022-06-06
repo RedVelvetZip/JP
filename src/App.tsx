1a8da7767b76fa3a50ba9d5e61f7b786e418560d
Bradley <bradley.t.sheehan@gmail.com>
2022-06-05 23:10:46 -0600

    tutor img

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './styles/main.scss'
 import hamburger from './assets/hamburger.png'
 import { useState } from 'react';
 // import logo from './assets/logo-short.png'
+import Navbar from './components/Navbar'
 
 type mindmapState = 'closed' | 'values' | 'products' | 'metaverse' | 'marketing'
 
@@ -15,73 +16,41 @@ function App() {
 
   return (
     <div className="App">
+     
       <div className="navbar">
-        <img
-          className="hamburger"
-          src={hamburger}
-          onClick={() => setActive(!active)}></img>
-        {active &&
-          <ul className="navbar-nav">
-            <li>
-              <a href="#" className="nav-link">
-                <span>Home</span>
-              </a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="#about" className="nav-link">
-                <span>About</span>
-              </a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="#rarity" className="nav-link">
-                <span>Rarity</span>
-              </a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="#roadmap" className="nav-link">
-                <span>Roadmap</span>
-              </a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="#mindmap" className="nav-link">
-                <span>Mindmap</span>
-              </a>
-            </li>
-            <li>|</li>
-            <li>
-              <a href="#team" className="nav-link">
-                <span>Team</span>
-              </a>
-            </li>
-          </ul>
-        }
+        <div className="tutorra">
+          {/* add logo */}
+          <p className="title">Tutorra</p>
+        </div>
+        <div>
+          <p>About</p>
+          <p>Courses</p>
+          <p>Pricing</p>
+        </div>
+        <div>
+          Contact Us
+        </div>
       </div>
       <section id="home" className="home">
         <div className="home-title">
           <h1 className="lltk">
-            LONG<br></br>LIVE<br></br>THE<br></br>KIKS
+            TUTORRA
           </h1>
         </div>
         <div className="home-char">
           <p>
-          [Company name] is the fastest way to get your child where they want to be.
+            [Company name] is the fastest way to get your child where they want to be.
 
-We offer a wide range of services, including SAT prep, tutoring, and help with high school. Our goal is to make sure that you and your child are on track for college acceptance—and we're confident that if you work with us, you'll be able to achieve a higher SAT score, improve grades, and more.
+            We offer a wide range of services, including SAT prep, tutoring, and help with high school. Our goal is to make sure that you and your child are on track for college acceptance—and we're confident that if you work with us, you'll be able to achieve a higher SAT score, improve grades, and more.
           </p>
         </div>
-        
+
       </section>
       <section id="values" className="values">
         <div className="values-title">
           <div className="values-text">
             <p>xxx</p>
           </div>
-        </div>
-        <div className="puzzle-two">
         </div>
       </section>
     </div>
@@ -90,6 +59,36 @@ We offer a wide range of services, including SAT prep, tutoring, and help with h
 
 export default App;
 
-function setActive(arg0: boolean): void {
-  throw new Error('Function not implemented.');
-}
+
+ {/* <ul className="navbar-nav">
+            <li>
+              <a href="#" className="nav-link">
+                <span>Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="nav-link">
+                <span>About</span>
+              </a>
+            </li>
+            <li>
+              <a href="#rarity" className="nav-link">
+                <span>Rarity</span>
+              </a>
+            </li>
+            <li>
+              <a href="#roadmap" className="nav-link">
+                <span>Roadmap</span>
+              </a>
+            </li>
+            <li>
+              <a href="#mindmap" className="nav-link">
+                <span>Mindmap</span>
+              </a>
+            </li>
+            <li>
+              <a href="#team" className="nav-link">
+                <span>Team</span>
+              </a>
+            </li>
+          </ul> */}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,16 @@ import * as React from 'react';
 export interface INavbarProps {
 }
 
-export function Navbar (props: INavbarProps) {
+export default function Navbar(props: INavbarProps) {
   return (
     <div>
-      
+      <div>
+        <p>Tutorra</p>
+      </div>
+      <div>
+        <p>About</p>
+        <p>Pricing</p>
+      </div>
     </div>
   );
 }
